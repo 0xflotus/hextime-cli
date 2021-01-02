8f309fd5bbfe317ccb0dcf7c0eef9b9d6a438d42
0xflotus <0xflotus@gmail.com>
2021-01-02 17:46:32 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+# hextime
+
+## Installation
+
+`yarn global add hextime-cli`
+
+## Usage
+
+`hextime`
--- index.js
+++ index.js
@@ -11,38 +11,20 @@ const argv = yargs(hideBin(process.argv))
     .argv
 
 
-var now = new Date();
+const now = new Date();
 const nowStr = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-var hours = now.getHours();
-var minutes = now.getMinutes();
-var seconds = now.getSeconds();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
 
 // http://www.intuitor.com/hex/hexclock.html
 function timeToHex(hh, mm, ss) {
-    var time = (hh * 3600 + mm * 60 + ss) * 65536 / 86400;
-    hexhours = convert(Math.floor(time / 4096));
+    const time = (hh * 3600 + mm * 60 + ss) * 65536 / 86400;
+    hexhours = Math.floor(time / 4096).toString(16).toUpperCase();
     hexminutes = Math.floor(time % 4096 / 16);
-    min1 = convert(Math.floor(hexminutes / 16));
-    min2 = convert(hexminutes % 16);
-    var hextimeValue = hexhours + "_" + min1 + min2;
-    return hextimeValue;
-}
-
-function convert(dec) {
-    var hex = dec
-    if (dec == 10)
-        hex = "A"
-    else if (dec == 11)
-        hex = "B"
-    else if (dec == 12)
-        hex = "C"
-    else if (dec == 13)
-        hex = "D"
-    else if (dec == 14)
-        hex = "E"
-    else if (dec == 15)
-        hex = "F"
-    return hex
+    min1 = Math.floor(hexminutes / 16).toString(16).toUpperCase();
+    min2 = (hexminutes % 16).toString(16).toUpperCase();
+    return hexhours + "_" + min1 + min2;
 }
 
 const hextimeValue = timeToHex(hours, minutes, seconds);
